bf97b34f77b58423ea3401c19aa3d96645737222
AlissaHorqque08 <alissa.horqque@hotmail.com>
2026-09-23 15:23:57 -0400

    Realized that the cat is in the wrong file, and changed the code to cat.js

--- Topics/cat/js/script.js
+++ Topics/cat/js/script.js
@@ -8,10 +8,8 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
+    createCanvas(400,400)
 
 }
 
@@ -20,5 +18,59 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(205,205,205)
 
+    strokeWeight(3);
+
+    drawCat();
 }
+
+
+function drawCat(){
+    drawCatEars();
+    drawLegs();
+    drawBody();
+    drawEyes();
+    drawMouth();
+}
+
+function drawCatEars(){
+    push();
+  triangle(120,30,120,130,200,130);
+
+  triangle(280,30,200,130,280,130);
+  pop();
+}
+
+function drawLegs(){
+  push();
+  triangle(120,300,150,310,120,350)
+
+triangle(240,300,280,300,280,350)
+
+triangle(145,300,180,310,165,350)
+  triangle(220,300,255,310,240,350)
+  
+}
+
+function drawBody(){
+    push();
+    ellipse(200,200,300,250);
+    pop();
+}
+
+function drawEyes(){
+  push();
+
+  strokeWeight(15);
+  point(135, 135);
+
+  point(233, 135);
+}
+
+function drawMouth(){
+  push();
+  strokeWeight(3);
+  spline(200, 145, 186, 160, 176, 160, 163, 145);
+}
+
--- Topics/hello-world/version-control-workflow/js/script.js
+++ Topics/hello-world/version-control-workflow/js/script.js
@@ -31,7 +31,7 @@ function drawCat(){
     drawCatEars();
     drawLegs();
     drawBody();
-    //drawEyes();
+    drawEyes();
     //drawMouth();
 }
 
