ad6e8c54c3bf55e9301947291828bd56414ea3c6
AlissaHorqque08 <alissa.horqque@hotmail.com>
2026-09-24 16:57:37 -0400

    Added Mrfurious turning, a bird, and make the background turn black as mrfurious grows more enraged

--- Topics/variables-challenge/js/script.js
+++ Topics/variables-challenge/js/script.js
@@ -1,24 +1,96 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Mr. Furious
+ * Pippin Barr
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
+
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
+
+};
+
+let sky = {
+    r: 204, 
+    g: 255, 
+    b: 255
+};
+
+let bird = {
+   x:50,
+   y:50,
+   width:50,
+   height:50,
+   speed: 1,
+
+
+    
+};
+
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Create the canvas
+ */
 function setup() {
-
+  createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
+  background(sky.r, sky.g, sky.b);
+    sky.g -= 1;
+    sky.b -= 1;
+    sky.r -= 1;
 
+    bird.x += bird.speed;
+
+  
+//   mrFurious.fill.r = lerp(mrFurious.fill.r, 255,0.1);
+  mrFurious.fill.g -= 1
+  mrFurious.fill.b -= 1
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  //mrFurious.fill.r += mrFurious.fill.r.speed();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+
+  mrFurious.y += random(1,0);
+    mrFurious.x += random(1,0);
+
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
+
+//   push();
+//   ellipse(CENTER);
+//   noStroke();
+//   ellipse(mrFurious.x, mrFurious.y + mrFurious.width *  mrFurious.height * 1.0)
+
+
+  push();
+  rectMode(CENTER);
+  noStroke();
+  fill("white");
+  circle(bird.x, bird.y + bird.width * 0.5, bird.height);
+  pop();
 }
+
+
+
