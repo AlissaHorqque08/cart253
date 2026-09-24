e917531feed1e3825ae34c50ea4e95401620729f
AlissaHorqque08 <alissa.horqque@hotmail.com>
2026-09-24 14:45:27 -0400

    Added bangs and a body

--- Topics/llama/js/script.js
+++ Topics/llama/js/script.js
@@ -23,4 +23,47 @@ function setup() {
 function draw() {
     background(255,38,38);
 
+    drawGirl();
+}
+
+function drawGirl(){
+    
+    drawBody();
+    drawHair();
+}
+
+function drawBody(){
+    push();
+    fill(38,255,212);  
+
+    //head
+    circle(280,115,150);
+    triangle(204, 130, 290,190, 235,190)
+
+    //neck
+    quad(270,235,  270,165,  320,165,  320,235)
+    quad(270,225, 320,205, 325,260, 245,260)
+
+    //body
+    quad(325,260,245,260, 235,300, 325,300)
+    quad(235,300,325,300, 325,400, 235,400)
+
+    //arm
+    circle(320,250,60)
+    quad(325,250, 350,250, 350,400, 325, 400)
+
+    pop();
+}
+
+function drawHair(){
+
+    push();
+    fill(255,255,255);
+    //bangs
+    triangle(240,160,280,90, 240,80)
+    triangle(280,90, 280,230, 320,90)
+    triangle(240,80, 210,70, 180,210)
+
+    
+
 }
